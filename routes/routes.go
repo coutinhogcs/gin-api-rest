47f2b24e92f4e741d492c5de34c02ec145eca964
Guilherme Coutinho <guilhermecoutslc@outlook.com>
2025-10-29 16:37:46 -0300

    Melhorando as rotas

--- routes/routes.go
+++ routes/routes.go
@@ -5,13 +5,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func HandleRequest() {
-	r := gin.Default()
+func SetupRoutes(r *gin.Engine) {
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/alunos/:id", controllers.ProcuraAlunoPorId)
 	r.GET("alunos/cpf/:cpf", controllers.BuscaPorCpf)
 	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.DELETE("/alunos/:id", controllers.DeletaAlunoPorId)
 	r.PATCH("alunos/:id", controllers.AtualizaAluno)
+}
+
+func HandleRequest() {
+	r := gin.Default()
+
+	// Chama a função de setup
+	SetupRoutes(r)
+
+	// Inicia o servidor
 	r.Run(":9000")
 }
